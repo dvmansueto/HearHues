ab3d04e56b48d54ca2403549a677d61692ba145a
dvmansueto <dvmansueto@gmail.com>
2016-10-14 02:12:52 +1030

    #8. Clean up fragment-based interface.

--- app/src/main/java/net/dvmansueto/hearhues/MainActivity.java
+++ app/src/main/java/net/dvmansueto/hearhues/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -62,7 +62,7 @@ public class MainActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_app_bar, menu);
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true;
     }
 
