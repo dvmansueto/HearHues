f5d8e4722045b0886488e96b5406c7e8956f1272
dvmansueto <dvmansueto@gmail.com>
2016-10-31 20:46:03 +1030

    #45. Substantial re-write, now takes a datum and then provides relative utility functions.

--- app/src/main/java/net/dvmansueto/hearhues/LocTone.java
+++ app/src/main/java/net/dvmansueto/hearhues/LocTone.java
@@ -1,76 +1,81 @@
 package net.dvmansueto.hearhues;
 
 import android.location.Location;
-import android.util.Log;
-
-import java.util.Locale;
 
 /**
  * LocTone: idea is to establish a datum, then use relative latitude and longitude to alter the
  * amplitude and frequency (or visa versa) of a generated tone.
  * Created by Dave on 27/10/16.
  */
-
 final class LocTone {
 
-    private static final String TAG = "LocTone";
+    /** Earth major radius, as defined by WGS84, in metres */
+    private static final double EQUATORIAL_RADIUS = 6378137.0;
 
-    private static final double AMPLITUDE_DISTANCE = 100; // metres from datum to max amp
-    private static final double FREQUENCY_DISTANCE = 100; // metres from datum to max freq
+    /** Earth semi-major radius as defined by WGS84, in metres */
+    private static final double POLAR_RADIUS = 6356752.3142;
 
-    private ScalarTone mScalarTone;
+    /** 'height' which bounds the 'view', in metres */
+    private double mLatitudeRange;
 
-    private double mLatitudeRadius = 6378137.0; // WGS84 major (equatorial) radius
-    private double mLongitudeRadius = 6356752.3142; // WGS84 semi-major (polar) radius
+    /** 'width' which bounds the 'view', in metres */
+    private double mLongitudeRange;
 
-    private double mRefLat;
-    private double mRefLong;
+    /** Latitude of origin */
+    private double mOriginLatitude;
 
-    private double mDeltaLat = 0;
-    private double mDeltaLong = 0;
+    /** Longitude at origin */
+    private double mOriginLongitude;
 
-    private double mAmplitude = 0;
-    private double mFrequency = -1;
+    /** longitudinal radius of the earth at datum latitude, in metres */
+    private double mLatitudinalRadius;
 
-    LocTone(Location datum) {
-        mRefLat = datum.getLatitude();
-        mRefLong = datum.getLongitude();
-        mLatitudeRadius = computeLatitudeRadius();
+
+    //--------------------------------
+    // Constructor
+    //--------------------------------
+
+    /**
+     * Creates a new LocTone.
+     * @param origin the 'datum' position
+     * @param latitudeRange the 'height' for valid coordinates, in metres
+     * @param longitudeRange the 'width' for valid coordinates, in metres
+     */
+    LocTone( double latitudeRange, double longitudeRange, Location origin) {
+        mLatitudeRange = latitudeRange;
+        mLongitudeRange = longitudeRange;
+        mOriginLatitude = origin.getLatitude();
+        mOriginLongitude = origin.getLongitude();
+        mLatitudinalRadius = computeLongitudinalRadius();
     }
 
-    void updateLoc( Location update) {
-        mDeltaLat = computeLatitudeDistance( update.getLatitude());
-        mDeltaLong = computeLongitudeDistance( update.getLongitude());
 
-        mAmplitude = deltaToScalar( mDeltaLat, AMPLITUDE_DISTANCE);
-        mFrequency = deltaToScalar( mDeltaLong, FREQUENCY_DISTANCE);
-    }
+    //--------------------------------
+    // Helpers
+    //--------------------------------
 
-    double getAmplitude() {
-        return mAmplitude;
-    }
-
-    double getFrequency() {
-        return mFrequency;
+    /**
+     * Calculates the scalar position of this latitude relative to the window.
+     * @param latitude the latitude to compare
+     * @return the position relative to the window [0...1]
+     */
+    double scalarLatitude(double latitude) {
+        return deltaToScalar( latitudeToDelta( latitude), mLatitudeRange);
     }
 
     /**
-     * Fetches the tone in frequency format.
-     * @return the tone as a "xxxxxx.xx Hz" format string.
+     * Calculates the scalar position of this longitude relative to the window.
+     * @param longitude the latitude to compare
+     * @return the position relative to the window [0...1]
      */
-    String toToneString() {
-
-        // Locale.getDefault() for appropriate '.' or ',' decimal point
-        return String.format(Locale.getDefault(), "%7.2f", mFrequency) + " Hz";
+    double scalarLongitude(double longitude) {
+        return deltaToScalar( longitudeToDelta( longitude), mLongitudeRange);
     }
 
-    /**
-     * Fetches the nearest half-note.
-     * @return the nearest half-note as a formatted string.
-     */
-    String toNoteString() {
-        return ( mFrequency < 0) ? "" : mScalarTone.toNoteString( mFrequency);
-    }
+
+    //--------------------------------
+    // Utilities
+    //--------------------------------
 
     /**
      * Converts a positive or negative delta to a [0...1] scalar centered about 0.5
@@ -89,44 +94,45 @@ final class LocTone {
     }
 
     /**
-     * Computes the distance between two latitudes.
+     * Computes the distance from this latitude to the origin.
      * @param latitude the new latitude, in degrees
-     * @return the distance between the new latitude and datum, in metres
+     * @return the distance between the new latitude and origin, in metres
      */
-    private double computeLatitudeDistance( double latitude) {
+    private double latitudeToDelta(double latitude) {
 
         // convert angular separation from degrees to radians
-        double angle = (latitude - mRefLat) * Math.PI / 180.0;
+        double angle = (latitude - mOriginLatitude) * Math.PI / 180.0;
 
         // arc length = angle * ( 2 * PI * radius)
-        return 2.0 * Math.PI * mLatitudeRadius * angle;
+        return 2.0 * Math.PI * mLatitudinalRadius * angle;
     }
 
     /**
-     * Computes the distance between two longitudes.
+     * Computes the distance from this longitude to the origin.
      * @param longitude the new longitude, in degrees
-     * @return the distance between the new longitude and datum, in metres
+     * @return the distance between the new longitude and origin, in metres
      */
-    private double computeLongitudeDistance( double longitude) {
-        double angle = ( longitude - mRefLong) * Math.PI / 180.0;
-        return 2.0 * Math.PI * mLongitudeRadius * angle;
+    private double longitudeToDelta(double longitude) {
+        double angle = ( longitude - mOriginLongitude) * Math.PI / 180.0;
+        return 2.0 * Math.PI * POLAR_RADIUS * angle;
     }
 
     /**
-     * Computes Earth's radius at mRefLat.
+     * Computes Earth's longitudinal radius at mOriginLatitude.
      *            _______________________
      *           / (a²cosφ)² + (b²sinφ)²
      *  R(φ) =  / ----------------------
      *         √  (a•cosφ)² + (b•sinφ)²
      *
-     * @return Earth's radius at mRefLat.
+     *  a = equatorial radius, b = polar radius
+     *
+     * @return Earth's radius at mOriginLatitude
      */
-    private double computeLatitudeRadius() {
-        double a = mLatitudeRadius; // probably a bad way of doing this, can only be used once
-        double b = mLongitudeRadius;
-        double cosP = Math.cos( mRefLat * Math.PI / 180.0);
-        double sinP = Math.sin( mRefLat * Math.PI / 180.0);
-
+    private double computeLongitudinalRadius() {
+        final double a = EQUATORIAL_RADIUS;
+        final double b = POLAR_RADIUS;
+        final double cosP = Math.cos( mOriginLatitude * Math.PI / 180.0);
+        final double sinP = Math.sin( mOriginLatitude * Math.PI / 180.0);
         return Math.sqrt( ( ( a * a * cosP) * ( a * a * cosP) + ( b * b * sinP) * ( b * b * sinP))
             / ( ( a * cosP) * ( a * cosP) + ( b * sinP) * ( b * sinP)));
     }
