e40984d62a2035a0681c3b3ea35017d6f62c615a
dvmansueto <dvmansueto@gmail.com>
2016-10-31 20:50:46 +1030

    #48. Tidying. Adapt to modified LocView.

--- app/src/main/java/net/dvmansueto/hearhues/TinkerFragment.java
+++ app/src/main/java/net/dvmansueto/hearhues/TinkerFragment.java
@@ -4,27 +4,44 @@ import android.media.AudioTrack;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A way for the user to muck around with things.
  */
 public class TinkerFragment extends Fragment {
 
-    private static final String TAG = "TinkerFragment";
+    /** Indicates the user is interacting with HueView */
+    private static final int HUE_VIEW = 1;
 
+    /** Indicates the user is interacting with LocView */
+    private static final int LOC_VIEW = 2;
+
+    /** Provides scalar:tone functions */
     private ScalarTone mScalarTone;
+
+    /** Displays a hue map for the user to click, interfaces with a HueTone */
+    //TODO: implement HueView
+    @SuppressWarnings({"FieldCanBeLocal", "unused"})
     private HueView mHueView;
+
+    /** Displays a plot for the user to click, makes cool noises */
+    @SuppressWarnings("FieldCanBeLocal")
     private LocView mLocView;
+
+    /** Generates tones. Is a global variable. */
     private ToneGenerator mToneGenerator;
 
+    /** Identifies which of the two views the user is interacting with */
     private int mCurrentView;
-    private static final int HUE_VIEW = 1;
-    private static final int LOC_VIEW = 2;
+
+
+    //------------------------
+    // Constructor
+    //------------------------
 
     public TinkerFragment() {
         // Required empty public constructor
@@ -45,18 +62,18 @@ public class TinkerFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_tinker, container, false);
     }
 
-
     @Override
     public void onResume() {
         super.onResume();
-        AppCompatActivity activity = (AppCompatActivity) getActivity();
-        activity.getSupportActionBar().setTitle(R.string.fragment_tinker_title);
 
-        mHueView = (HueView) activity.findViewById( R.id.tinker_hue_view);
+        //noinspection ConstantConditions
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.fragment_tinker_title);
 
-        ApplicationSingleton applicationSingleton = (ApplicationSingleton) getActivity().getApplicationContext();
-        mScalarTone = applicationSingleton.getScalarTone();
-        mToneGenerator = applicationSingleton.getToneGenerator();
+        mHueView = (HueView) getActivity().findViewById( R.id.tinker_hue_view);
+
+        ApplicationContext applicationContext = (ApplicationContext) getActivity().getApplicationContext();
+        mScalarTone = applicationContext.getScalarTone();
+        mToneGenerator = applicationContext.getToneGenerator();
         mToneGenerator.setToneGeneratorListener( new ToneGenerator.ToneGeneratorListener() {
             @Override
             public void startedPlaying() {
@@ -78,28 +95,21 @@ public class TinkerFragment extends Fragment {
         mLocView.setTouchAllowed( true);
         mLocView.setLocViewListener(new LocView.LocViewListener() {
             @Override
-            public void newFrequency(double frequency) {
+            public void newScalarCoords( double[] newScalarCoords) {
                 if ( mCurrentView != LOC_VIEW) setCurrentView( LOC_VIEW);
-                mToneGenerator.setFrequency( mScalarTone.scalarToTone( frequency));
-//                mToneGenerator.play();
-            }
-            @Override
-            public void newAmplitude(double amplitude) {
-                if ( mCurrentView != LOC_VIEW) setCurrentView( LOC_VIEW);
-                mToneGenerator.setAmplitude( amplitude);
-//                mToneGenerator.play();
+                mToneGenerator.setAmplitude( newScalarCoords[ 1]); // amp first as freq plays
+                mToneGenerator.setFrequency( mScalarTone.scalarToTone( newScalarCoords[ 0]));
             }
         });
     }
-
 
     @Override
     public void onPause() {
         super.onPause();
         if ( mCurrentView != HUE_VIEW) setCurrentView( HUE_VIEW); // restore normal activity.
-        ApplicationSingleton applicationSingleton = (ApplicationSingleton) getActivity().getApplicationContext();
-        applicationSingleton.setScalarTone( mScalarTone);
-        applicationSingleton.setToneGenerator( mToneGenerator);
+        ApplicationContext applicationContext = (ApplicationContext) getActivity().getApplicationContext();
+        applicationContext.setScalarTone( mScalarTone);
+        applicationContext.setToneGenerator( mToneGenerator);
     }
 
     @Override
@@ -124,6 +134,9 @@ public class TinkerFragment extends Fragment {
         }
     }
 
+    /**
+     * Optimises the Tone Generator for looped short bursts.
+     */
     private void optimiseForLocView() {
         mToneGenerator.setPlaybackMode ( AudioTrack.MODE_STREAM);
         mToneGenerator.setPlaybackFactor( 0.0625); // much shorter playback times
@@ -132,6 +145,9 @@ public class TinkerFragment extends Fragment {
         mToneGenerator.setRampingDown( true);
     }
 
+    /**
+     * Optimises the Tone Generator for single long notes.
+     */
     private void optimiseForHueView() {
         mToneGenerator.setPlaybackMode ( AudioTrack.MODE_STATIC);
         mToneGenerator.setPlaybackFactor( 1); // much shorter playback times
