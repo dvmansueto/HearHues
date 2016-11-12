ce85e1d4f05e564176a69b989de9377ca31e2cb9
dvmansueto <dvmansueto@gmail.com>
2016-11-12 16:08:52 +1030

    #61. Updated `LocTone`:   * Replaced origin lat/long doubles with a Location   * Added memory of current Location   * Added `setLocation()`   * Converted `double latitudeToScalar( double latitude)` to `double getScalarLatitude()`, similarly for longitude   * Added `toDegreeString()` and `toCoordString()`

--- app/src/main/java/net/dvmansueto/hearhues/LocTone.java
+++ app/src/main/java/net/dvmansueto/hearhues/LocTone.java
@@ -2,6 +2,8 @@ package net.dvmansueto.hearhues;
 
 import android.location.Location;
 
+import java.util.Locale;
+
 /**
  * LocTone: idea is to establish a datum, then use relative latitude and longitude to alter the
  * amplitude and frequency (or visa versa) of a generated tone.
@@ -21,14 +23,15 @@ final class LocTone {
     /** 'width' which bounds the 'view', in metres */
     private final double mLongitudeRange;
 
-    /** Latitude of origin */
-    private final double mOriginLatitude;
-
-    /** Longitude at origin */
-    private final double mOriginLongitude;
-
     /** longitudinal radius of the earth at datum latitude, in metres */
     private final double mLatitudinalRadius;
+
+    /** The origin/datum {@link Location} */
+    private final Location mOrigin;
+
+    /** The present/current {@link Location} */
+    private Location mLocation;
+
 
 
     //--------------------------------
@@ -44,32 +47,56 @@ final class LocTone {
     LocTone( double latitudeRange, double longitudeRange, Location origin) {
         mLatitudeRange = latitudeRange;
         mLongitudeRange = longitudeRange;
-        mOriginLatitude = origin.getLatitude();
-        mOriginLongitude = origin.getLongitude();
+        mLocation = mOrigin = origin;
         mLatitudinalRadius = computeLongitudinalRadius();
     }
 
 
     //--------------------------------
-    // Helpers
+    // Mutators
+    //--------------------------------
+
+    /**
+     * Updates the {@link LocTone} with a new {@link Location}.
+     * @param location the new location
+     */
+    void setLocation( Location location) {
+        mLocation = location;
+    }
+
+
+    //--------------------------------
+    // Accessors
     //--------------------------------
 
     /**
      * Calculates the scalar position of this latitude relative to the window.
-     * @param latitude the latitude to compare
      * @return the position relative to the window [0...1]
      */
-    double scalarLatitude(double latitude) {
-        return deltaToScalar( latitudeToDelta( latitude), mLatitudeRange);
+    double getScalarLatitude() {
+        return deltaToScalar( latitudeToDelta( mLocation.getLatitude()), mLatitudeRange);
     }
 
     /**
      * Calculates the scalar position of this longitude relative to the window.
-     * @param longitude the latitude to compare
      * @return the position relative to the window [0...1]
      */
-    double scalarLongitude(double longitude) {
-        return deltaToScalar( longitudeToDelta( longitude), mLongitudeRange);
+    double getScalarLongitude() {
+        return deltaToScalar( longitudeToDelta( mLocation.getLongitude()), mLongitudeRange);
+    }
+
+    /**
+     * Returns the location formatted as a decimal-degree string
+     * @return the decimal-degree "latitude, longitude" string
+     */
+    String toDegreeString() {
+        return Location.convert( mLocation.getLatitude(), Location.FORMAT_DEGREES) + ", " +
+                Location.convert( mLocation.getLongitude(), Location.FORMAT_DEGREES);
+    }
+
+    String toCoordString() {
+        return String.format(Locale.getDefault(), "%5.2f %5.2f",
+                latitudeToDelta( mLocation.getLatitude()), longitudeToDelta( mLocation.getLongitude()));
     }
 
 
@@ -101,7 +128,7 @@ final class LocTone {
     private double latitudeToDelta(double latitude) {
 
         // convert angular separation from degrees to radians
-        double angle = (latitude - mOriginLatitude) * Math.PI / 180.0;
+        double angle = (latitude - mOrigin.getLatitude()) * Math.PI / 180.0;
 
         // arc length = angle * ( 2 * PI * radius)
         return 2.0 * Math.PI * mLatitudinalRadius * angle;
@@ -113,7 +140,7 @@ final class LocTone {
      * @return the distance between the new longitude and origin, in metres
      */
     private double longitudeToDelta(double longitude) {
-        double angle = ( longitude - mOriginLongitude) * Math.PI / 180.0;
+        double angle = ( longitude - mOrigin.getLongitude()) * Math.PI / 180.0;
         return 2.0 * Math.PI * POLAR_RADIUS * angle;
     }
 
@@ -131,8 +158,8 @@ final class LocTone {
     private double computeLongitudinalRadius() {
         final double a = EQUATORIAL_RADIUS;
         final double b = POLAR_RADIUS;
-        final double cosP = Math.cos( mOriginLatitude * Math.PI / 180.0);
-        final double sinP = Math.sin( mOriginLatitude * Math.PI / 180.0);
+        final double cosP = Math.cos( mOrigin.getLatitude() * Math.PI / 180.0);
+        final double sinP = Math.sin( mOrigin.getLatitude() * Math.PI / 180.0);
         return Math.sqrt( ( ( a * a * cosP) * ( a * a * cosP) + ( b * b * sinP) * ( b * b * sinP))
             / ( ( a * cosP) * ( a * cosP) + ( b * sinP) * ( b * sinP)));
     }
