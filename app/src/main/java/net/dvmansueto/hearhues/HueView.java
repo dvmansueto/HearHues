ea070c334758eaa8d47d18a5433d3525464917ae
dvmansueto <dvmansueto@gmail.com>
2016-11-13 14:05:11 +1030

    #65. Basic implementation of `HueView`: added touch and listener, just missing tracking cursor.

--- app/src/main/java/net/dvmansueto/hearhues/HueView.java
+++ app/src/main/java/net/dvmansueto/hearhues/HueView.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v4.graphics.ColorUtils;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -14,18 +15,45 @@ import android.view.View;
  */
 public class HueView extends View {
 
+    /**
+     * The maximum hue for HSL (wraps around beyond 360°)
+     * Also defines number of hue steps
+     * */
     private static final int HUE_MAX = 360;
+
+    /**
+     * The maximum luminance for HSL (wraps around beyond 1)
+     * Also defines number of luminance steps
+     * */
     private static final int LUM_MAX = 100;
 
     /** Constant maximum saturation */
     private static final float SAT = 1;
 
-    private float mDp;
-
+    /** Defines the colour using HSL at ( x, y) */
     private Paint[][] mHlPaint;
 
+    /** Scales {@link #HUE_MAX} to view width 'x' */
     private float mHueToX;
+
+    /** Scales {@link #LUM_MAX} to view height 'y' */
     private float mLumToY;
+
+    /** The display-dependent width of the view, in pixels */
+    private int mWidth;
+
+    /** The display-dependent height of the view, in pixels */
+    private int mHeight;
+
+    private HueViewListener mHueViewListener;
+
+    public interface HueViewListener {
+        void newScalarCoords( double[] scalarCoords);
+    }
+
+    public void setHueViewListener( HueViewListener hueViewListener) {
+        mHueViewListener = hueViewListener;
+    }
 
     /**
      * Constructs a new HueView.
@@ -35,9 +63,6 @@ public class HueView extends View {
      */
     public HueView(Context context, AttributeSet attributes) {
         super(context, attributes);
-
-        // to convert to display-independent pixels (dip/dp)
-        mDp = getResources().getDisplayMetrics().density;
 
         mHlPaint = new Paint[ HUE_MAX][ LUM_MAX];
         // for each hue in x (horizontal, left to right)
@@ -85,19 +110,56 @@ public class HueView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
 
         // hue * scale = width ==> scale = width / hue
-        mHueToX = ( (float) w) / HUE_MAX;
-        mLumToY = ( (float) h) / LUM_MAX;
+        mHueToX = ((float) w) / HUE_MAX;
+        mLumToY = ((float) h) / LUM_MAX;
 
-//        mIsPortrait = mHeight > mWidth;
+        mWidth = w;
+        mHeight = h;
+    }
 
+    /**
+     * Interprets touch, updates old and new coordinates, and throws frequency and amplitude
+     * percentages.
+     * @param x the horizontal position
+     * @param y the vertical position
+     */
+    private void parseTouch( float x, float y) {
+        // impose limits since you can drag out-of-bounds of the view...
+        if ( x < 0) x = 0;
+        if ( x > mWidth) x = mWidth;
+        if ( y < 0) y = 0;
+        if ( y > mHeight) y = mHeight;
 
+//        newAbsoluteCoords( x, y);
 
-//        for ( int i = 0; i < OLD_COORD_COUNT; i++) {
-//            mOldCoordRadii[ i] *= mDp;
-//        }
-//        mNewCoordRadius = NEW_COORD_RADIUS * mDp;
-//
-//        updateAxes();
+        // view is top-down, coords are bottom-up...
+        if ( mHueViewListener != null ) mHueViewListener.newScalarCoords(
+                new double[] { x / mWidth, ( mHeight - y) / mHeight});
+    }
+
+    /**
+     * Switches the motion event, passing touch x & y coordinates to
+     * {@link #parseTouch(float, float)}.
+     * @param motionEvent the incoming touch event
+     * @return true if the x & y coords were passed to parseTouch.
+     */
+    @Override
+    public boolean onTouchEvent( MotionEvent motionEvent) {
+
+        // shamelessly stolen from
+        // https://examples.javacodegeeks.com/android/core/graphics/canvas-graphics/android-canvas-example/
+        float x = motionEvent.getX();
+        float y = motionEvent.getY();
+
+        switch(motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                parseTouch(x, y);
+                invalidate();
+                return true;
+        }
+        return false;
     }
 
 }
