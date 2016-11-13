250eea085a150a1ec53467d80a0dfe53344864fd
dvmansueto <dvmansueto@gmail.com>
2016-11-13 14:28:22 +1030

    #67. `TinkerFragment`: adapt for updated `HueView`

--- app/src/main/java/net/dvmansueto/hearhues/TinkerFragment.java
+++ app/src/main/java/net/dvmansueto/hearhues/TinkerFragment.java
@@ -2,6 +2,7 @@ package net.dvmansueto.hearhues;
 
 import android.media.AudioTrack;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,29 +16,18 @@ import android.view.ViewGroup;
  */
 public class TinkerFragment extends Fragment {
 
-    /** Indicates the user is interacting with HueView */
-    private static final int HUE_VIEW = 1;
-
-    /** Indicates the user is interacting with LocView */
-    private static final int LOC_VIEW = 2;
-
     /** Provides scalar:tone functions */
     private ScalarTone mScalarTone;
 
     /** Displays a hue map for the user to click, interfaces with a HueTone */
     //TODO: implement HueView
-    @SuppressWarnings({"FieldCanBeLocal", "unused"})
     private HueView mHueView;
 
     /** Displays a plot for the user to click, makes cool noises */
-    @SuppressWarnings("FieldCanBeLocal")
     private LocView mLocView;
 
     /** Generates tones. Is a global variable. */
     private ToneGenerator mToneGenerator;
-
-    /** Identifies which of the two views the user is interacting with */
-    private int mCurrentView;
 
 
     //------------------------
@@ -58,6 +48,16 @@ public class TinkerFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // in onViewCreated to try and reduce number of initialisation tasks
+        mHueView = (HueView) view.findViewById( R.id.tinker_hue_view);
+        mLocView = (LocView) view.findViewById( R.id.tinker_loc_view);
+
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
 
@@ -66,11 +66,10 @@ public class TinkerFragment extends Fragment {
         actionBar.setIcon( R.drawable.ic_tinker_24);
         actionBar.setTitle( R.string.fragment_tinker_title);
 
-        mHueView = (HueView) getActivity().findViewById( R.id.tinker_hue_view);
-
         ApplicationContext applicationContext = (ApplicationContext) getActivity().getApplicationContext();
         mScalarTone = applicationContext.getScalarTone();
         mToneGenerator = applicationContext.getToneGenerator();
+        mToneGenerator.setStreamMode(); // continuous stream of short tones
         mToneGenerator.setToneGeneratorListener( new ToneGenerator.ToneGeneratorListener() {
             @Override
             public void startedPlaying() {
@@ -88,63 +87,30 @@ public class TinkerFragment extends Fragment {
             }
         });
 
-        mLocView = (LocView) getActivity().findViewById( R.id.tinker_loc_view);
+        mHueView.setHueViewListener( new HueView.HueViewListener() {
+                 @Override
+                 public void newScalarCoords(double[] scalarCoords) {
+                     mToneGenerator.setAmplitude( scalarCoords[ 1]);
+                     mToneGenerator.setFrequency( mScalarTone.scalarToTone( scalarCoords[ 0]));
+                 }
+         });
+
         mLocView.setTouchAllowed( true);
         mLocView.setLocViewListener(new LocView.LocViewListener() {
-            @Override
-            public void newScalarCoords( double[] newScalarCoords) {
-                if ( mCurrentView != LOC_VIEW) setCurrentView( LOC_VIEW);
-                mToneGenerator.setAmplitude( newScalarCoords[ 1]); // amp first as freq plays
-                mToneGenerator.setFrequency( mScalarTone.scalarToTone( newScalarCoords[ 0]));
-            }
+                @Override
+                public void newScalarCoords( double[] scalarCoords) {
+                    mToneGenerator.setAmplitude( scalarCoords[ 1]); // amp first as freq plays
+                    mToneGenerator.setFrequency( mScalarTone.scalarToTone( scalarCoords[ 0]));
+                }
         });
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if ( mCurrentView != HUE_VIEW) setCurrentView( HUE_VIEW); // restore normal activity.
         ApplicationContext applicationContext = (ApplicationContext) getActivity().getApplicationContext();
         applicationContext.setScalarTone( mScalarTone);
         applicationContext.setToneGenerator( mToneGenerator);
     }
 
-    /**
-     * Allows for changing ToneGenerator parameters based on view user is interacting with.
-     * @param view the view to optimise for.
-     */
-    private void setCurrentView( int view) {
-        switch ( view) {
-            case HUE_VIEW:
-                optimiseForHueView();
-                mCurrentView = HUE_VIEW;
-                break;
-            case LOC_VIEW:
-                optimiseForLocView();
-                mCurrentView = LOC_VIEW;
-                break;
-        }
-    }
-
-    /**
-     * Optimises the Tone Generator for looped short bursts.
-     */
-    private void optimiseForLocView() {
-        mToneGenerator.setPlaybackMode ( AudioTrack.MODE_STREAM);
-        mToneGenerator.setPlaybackFactor( 0.0625); // much shorter playback times
-        mToneGenerator.setPlayContinuously( true);
-        mToneGenerator.setRampingUp( true);
-        mToneGenerator.setRampingDown( true);
-    }
-
-    /**
-     * Optimises the Tone Generator for single long notes.
-     */
-    private void optimiseForHueView() {
-        mToneGenerator.setPlaybackMode ( AudioTrack.MODE_STATIC);
-        mToneGenerator.setPlaybackFactor( 1); // much shorter playback times
-        mToneGenerator.setPlayContinuously( false);
-        mToneGenerator.setRampingUp( true);
-        mToneGenerator.setRampingDown( true);
-    }
 }
